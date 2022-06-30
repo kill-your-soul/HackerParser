5e0fee97688422a404e6630280ab743a12c28975
kill-your-soul <platonovdi@gmail.com>
2022-06-30 22:25:30 +0300

    fixed loop wich download all reports and remove unnecessary imports

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import random
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webelement import WebElement
-from typing import List
+
 
 
 chromeOptions = webdriver.ChromeOptions()
@@ -26,8 +25,12 @@ password.send_keys(os.environ["password"])
 sleep(random.randint(1, 3))
 login_button = driver.find_element(by=By.ID, value="wp-submit")
 login_button.click()
-all_pages = driver.find_element(by=By.CLASS_NAME, value="pages")
-all_pages = int(all_pages.text[-2::])
-for i in range(1, all_pages):
+last_issue = driver.find_element(by=By.CLASS_NAME, value="entry-title")
+last_issue = int(
+    last_issue.find_element(by=By.TAG_NAME, value="a")
+    .get_attribute("href")
+    .split("/")[-1]
+)
+for i in range(1, last_issue):
     driver.get(f"https://xakep.ru/pdf/xa/{i:03}")
     sleep(1)
